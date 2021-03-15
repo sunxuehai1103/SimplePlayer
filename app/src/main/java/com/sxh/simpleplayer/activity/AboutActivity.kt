783a536d0a379720eab46c92abc3f9bc221fc976
sxh <sxh@hhdd.com>
2021-03-15 20:00:48 +0800

    设置页面、关于页面完成

---
+++ app/src/main/java/com/sxh/simpleplayer/activity/AboutActivity.kt
@@ -0,0 +1,15 @@
+package com.sxh.simpleplayer.activity
+
+import com.sxh.simpleplayer.R
+import com.sxh.simpleplayer.base.BaseActivity
+
+
+/**
+ * ClassName:AboutActivity
+ * Description:
+ */
+class AboutActivity: BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_about
+    }
+}
